c769224ad26f3e0cd274831bf784777a18085923
GusFurtado <gustavofurtado2@gmail.com>
2025-04-02 21:26:50 -0300

    ✅ Update tests with error handling

--- tests/test_cellmate.py
+++ tests/test_cellmate.py
@@ -1,18 +1,20 @@
+import pytest
+
 import cellmate as xl
-from cellmate import defaults
+from cellmate import styles
 from cellmate.errors import CellmateException
 
 
 def test_multisheet_workbook():
     columns1 = [
-        xl.Column(["A", "B"], title="str", title_style=defaults.TITLE1),
-        xl.Column([1, 2], title="int", title_style=defaults.TITLE2)
+        xl.Column(["A", "B"], title="str", title_style=styles.TITLE1),
+        xl.Column([1, 2], title="int", title_style=styles.TITLE2)
     ]
     sheet1 = xl.Sheet(columns1, sheet_name="multisheet1")
 
     columns2 = [
-        xl.Column([0.5, 1.5], title="currency", content_style=defaults.CURRENCY),
-        xl.Column([0.5, 1.5], title="percentage", content_style=defaults.PERCENTAGE)
+        xl.Column([0.5, 1.5], title="currency", content_style=styles.CURRENCY),
+        xl.Column([0.5, 1.5], title="percentage", content_style=styles.PERCENTAGE)
     ]
     sheet2 = xl.Sheet(columns2, sheet_name="multisheet2")
 
@@ -22,19 +24,18 @@ def test_multisheet_workbook():
 
 def test_singlesheet_workbook():
     columns = [
-        xl.Column([46000, 47000], title="date", content_style=defaults.DATE),
-        xl.Column([0.5, 1.5], title="int", content_style=defaults.INTEGER)
+        xl.Column([46000, 47000], title="date", content_style=styles.DATE),
+        xl.Column([0.5, 1.5], title="int", content_style=[styles.INTEGER, styles.INTEGER]),
     ]
     sheet = xl.Sheet(columns, sheet_name="singlesheet")
     sheet.save("test_singlesheet_workbook.xlsx")
 
 
 def test_error():
-    try:
-        column = xl.Column(
-            content=[1, 2],
+    with pytest.raises(CellmateException) as err:
+        xl.Column(
+            content=[1, 2, 3],
             title="default",
-            content_style=[defaults.DEFAULT, defaults.PERCENTAGE, defaults.PERCENTAGE_POINTS]
+            content_style=[styles.DEFAULT, styles.PERCENTAGE]
         )
-    except CellmateException as err:
-        pass
+    assert str(err.value) == "Content style length does not match content length"
